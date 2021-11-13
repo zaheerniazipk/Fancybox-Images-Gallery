47b1781abf6580ac2b19c252d561bc38c0f0c63c
zaheerniazipk <zaheerniazipak@gmail.com>
2021-11-13 22:52:09 +0500

    Fancybox Images Gallery

---
+++ script.js
@@ -0,0 +1,12 @@
+$('[data-fancybox]').fancybox({
+  loop: true,
+  buttons: [
+    "zoom",
+    "slideShow",
+    "fullScreen",
+    "thumbs",
+    "close"
+  ],
+  animationEffect: "zoom-in-out",
+  transitionEffect: "zoom-in-out"
+});
